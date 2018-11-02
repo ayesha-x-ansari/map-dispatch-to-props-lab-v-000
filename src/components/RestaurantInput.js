cebcf313e9b4798828cfbc2cc9713f63d629d534
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-02 21:06:32 +0000

    automatically backed up by learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -34,15 +34,15 @@ export class RestaurantInput extends Component {
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnNameChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
             placeholder="restaurant name" />
         </p>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnLocationChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -56,6 +56,7 @@ const mapDispatchToProps = (dispatch) => {
     addRestaurant,
   }, dispatch);
 };
+
 //export const ConnectedRestaurantInput = connect(null, mapDispatchToProps)(RestaurantInput);
 
 export default  connect(null, mapDispatchToProps)(RestaurantInput);
